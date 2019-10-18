d4fd9e4d68fa2cf5a73307cfd87367a9116cfd48
chenhailong <hailong.chen@xgo.one>
2019-10-18 17:15:08 +0800

    '数据库配置加入环境变量'

--- application/database.php
+++ application/database.php
@@ -9,19 +9,21 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
+use think\Env;
+
 return [
     // 数据库类型
     'type'           => 'mysql',
     // 服务器地址
-    'hostname'       => '127.0.0.1',
+    'hostname'       => Env::get('database.hostname'),
     // 数据库名
-    'database'       => 'easy_taobao',
+    'database'       => Env::get('database.database'),
     // 用户名
-    'username'       => 'root',
+    'username'       => Env::get('database.username'),
     // 密码
-    'password'       => 'root',
+    'password'       => Env::get('database.password'),
     // 端口
-    'hostport'       => '3306',
+    'hostport'       => Env::get('database.hostport'),
     // 连接dsn
     'dsn'            => '',
     // 数据库连接参数
